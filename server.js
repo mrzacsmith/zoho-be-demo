b8ac1af5246d8b18ba108379c57149a68073c963
Zac Smith <zrs3141592@gmail.com>
2020-10-21 21:20:15 -0600

    server running

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
 
 require('dotenv').config()
 require('colors')
 
 const server = express()
+server.use(cors())
+server.use(helmet())
+server.use(morgan('dev'))
+
+const currentTime = new Date().toLocaleString()
+
+// @desc		Test server is working
+// @route		/
+server.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Server is live!',
+    dataTime: currentTime + ' MST',
+    author: 'GitHub: @MrZacSmith',
+  })
+})
 
 const PORT = process.env.PORT || 3550
 server.listen(PORT, () => {
