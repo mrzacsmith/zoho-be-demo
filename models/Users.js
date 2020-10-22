f05221e99507b1ddf78907d7185e46aa84d3f690
Zac Smith <zrs3141592@gmail.com>
2020-10-21 21:41:59 -0600

    create model

---
+++ models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String },
+    lastName: { type: String },
+    street: { type: String },
+    city: { type: String },
+    state: { type: String },
+    zip: { type: Number },
+    message: { type: String, trim: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', UserSchema)
